00e07fa2cd28693424def286dc46e89397246c02
0583203077 <r0583203077@gmail.com>
2026-02-25 13:00:22 +0200

    Refactor onBallClick to support multiple balls

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-function onBallClick() {
-    var elBall = document.querySelector(".ball");
+function onBallClick(elBall, maxDiameter) {
+    // var elBall = document.querySelector(".ball");
     var currentWidth = parseInt(getComputedStyle(elBall).width);
     var randomNumber = getRandomInt()
-    elBall.style.width = (currentWidth + randomNumber) + "px";
-    if (currentWidth + 50 > 400)
-        elBall.style.width = 100 + "px"
-    var currentHeight = parseInt(getComputedStyle(elBall).height);
-    elBall.style.height = (currentHeight + randomNumber) + "px";
-    if (currentHeight + 50 > 400)
-        elBall.style.height = 100 + "px"
-    elBall.style.backgroundColor=getRandomColor();
+    var radius = randomNumber + currentWidth
+    if (radius > maxDiameter)
+        radius = 100;
+    console.log(randomNumber,radius,maxDiameter)
+    elBall.style.width = radius + "px";
+    elBall.style.height = radius + "px";
+
+    elBall.style.backgroundColor = getRandomColor();
 }
