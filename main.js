3f7e9872aa50a8b38ce1ba492ca5f19c71e83e3f
0583203077 <r0583203077@gmail.com>
2026-04-20 12:46:21 +0300

    Arrange the code

--- main.js
+++ main.js
@@ -1,17 +1,98 @@
 
-var balls;
-var undoStates = [];
-var redoStates = [];
+let balls;
+let undoStates = [];
+let redoStates = [];
 let moveCount = 0
 let seconds = 0
 let timerId = null
+let intervalId;
+let intervalCounter = 0
+
+function init() {
+    document.title = `Ball Game (${moveCount})`
+    const undoBtn = document.querySelector(".undo-btn");
+    const redoBtn = document.querySelector(".redo-btn");
+    const ball6 = document.querySelector(".ball6");
+    let hoverTimer = null;
+
+    undoBtn.disabled = true
+    redoBtn.disabled = true
+
+    ball6.addEventListener("mouseenter", () => {
+        hoverTimer = setTimeout(() => {
+            startClickHandlers()
+            intervalCounter = 0
+        }, 2000);
+    });
+
+    ball6.addEventListener("mouseleave", () => {
+        clearTimeout(hoverTimer);
+        clearInterval(intervalId);
+    });
+}
 
 init();
 
 function onBallClick(elBall, maxDiameter) {
-    undoStates.push(saveState())
+    saveAndUpdate()
     changeBall(elBall, maxDiameter)
-    updateCounter()
+}
+
+function onBall3Click() {
+    saveAndUpdate();
+    const { elBall1, elBall2 } = getBalls();
+    changeBall(elBall1, 300)
+    changeBall(elBall2, 500)
+}
+
+function onBall4Click() {
+    saveAndUpdate();
+    const { elBall1, elBall2 } = getBalls();
+    changeBall(elBall1, 0)
+    changeBall(elBall2, 0)
+}
+
+function onBall5Click() {
+    saveAndUpdate();
+    const body = document.querySelector("body");
+    body.style.backgroundColor = getRandomColor();
+    const undoBtn = document.querySelector(".undo-btn");
+    undoBtn.disabled = false
+}
+
+function onBall6Click() {
+    location.reload();
+    stopTimer()
+    const timerLabel = document.querySelector(".timer");
+    timerLabel.textContent = seconds
+}
+
+function undo(elUndo) {
+    var lastState = undoStates.pop();
+
+    if (undoStates.length === 0) {
+        elUndo.disabled = true
+    }
+    const redoBtn = document.querySelector(".redo-btn");
+    redoBtn.disabled = false
+    redoStates.push(saveState())
+    applyState(lastState);
+}
+
+function redo(elRedo) {
+    if (redoStates.length != 0) {
+        const lastState = redoStates.pop();
+        elRedo.disabled = (redoStates.length === 0)
+        undoStates.push(saveState())
+        const undoBtn = document.querySelector(".undo-btn");
+        undoBtn.disabled = (undoStates.length === 0)
+        applyState(lastState);
+    }
+}
+
+function saveAndUpdate() {
+    undoStates.push(saveState());
+    updateCounter();
 }
 
 function updateCounter() {
@@ -27,7 +108,6 @@ function startTimer() {
 
     timerId = setInterval(function () {
         seconds++
-        console.log(seconds)
         const timerLabel = document.querySelector(".timer");
         timerLabel.textContent = seconds
     }, 1000)
@@ -39,106 +119,36 @@ function stopTimer() {
 }
 
 function changeBall(elBall, maxDiameter) {
-    var currentWidth = parseInt(getComputedStyle(elBall).width);
-    console.log(undoStates)
-    const undoBtn = document.querySelector(".undo-btn");
+    let currentWidth = parseInt(getComputedStyle(elBall).width);
+    let undoBtn = document.querySelector(".undo-btn");
     undoBtn.disabled = false
 
-    var randomNumber = getRandomInt()
-    var radius = maxDiameter ? randomNumber + currentWidth : currentWidth - randomNumber
+    let randomNumber = getRandomInt()
+    let radius = maxDiameter ? randomNumber + currentWidth : currentWidth - randomNumber
     if ((maxDiameter && radius > maxDiameter) || radius < 100)
         radius = 100;
 
-    console.log(randomNumber, radius, maxDiameter)
     elBall.style.width = radius + "px";
     elBall.style.height = radius + "px";
-    var background = getRandomColor();
-    elBall.style.backgroundColor = background
+    elBall.style.backgroundColor = getRandomColor();
 }
-
-function onBall3Click() {
-    updateCounter()
-    undoStates.push(saveState())
-    var elBall1 = document.querySelector(".ball1");
-    var elBall2 = document.querySelector(".ball2");
-    changeBall(elBall1, 300)
-    changeBall(elBall2, 500)
-}
-
-function onBall4Click() {
-    updateCounter()
-    undoStates.push(saveState())
-    var elBall1 = document.querySelector(".ball1");
-    var elBall2 = document.querySelector(".ball2");
-    changeBall(elBall1, 0)
-    changeBall(elBall2, 0)
-}
-
-function onBall5Click() {
-    updateCounter()
-    var body = document.querySelector("body");
-    body.style.backgroundColor = getRandomColor();
-    undoStates.push(saveState())
-    const undoBtn = document.querySelector(".undo-btn");
-    undoBtn.disabled = false
-}
-
-function onBall6Click() {
-    // undoStates.push(saveState())
-    // moveCount++
-    // document.title = `Ball Game (${moveCount})`
-
-    location.reload();
-    stopTimer()
-const timerLabel = document.querySelector(".timer");
-        timerLabel.textContent = seconds
-}
-let intervalId;
-function init() {
-    document.title = `Ball Game (${moveCount})`
-    const undoBtn = document.querySelector(".undo-btn");
-    undoBtn.disabled = true
-    const redoBtn = document.querySelector(".redo-btn");
-    redoBtn.disabled = true
-    const ball6 = document.querySelector(".ball6");
-    let hoverTimer = null;
-
-    ball6.addEventListener("mouseenter", () => {
-        hoverTimer = setTimeout(() => {
-            console.log("Hover מעל 2 שניות");
-            startClickHandlers()
-            intervalCounter = 0
-        }, 2000);
-    });
-
-    ball6.addEventListener("mouseleave", () => {
-        clearTimeout(hoverTimer);
-        clearInterval(intervalId);
-    });
-}
-
-let intervalCounter = 0
-
-
 
 function startClickHandlers() {
-    intervalId = setInterval(function () {
+    intervalId = setInterval(() => {
         intervalCounter++
         if (intervalCounter === 10) {
             clearInterval(intervalId);
             console.log("האינטרוול נעצר", intervalCounter);
         }
         console.log("רץ כל 2 שניות", intervalCounter);
-        updateCounter()
-        undoStates.push(saveState())
+        saveAndUpdate();
         onBall3Click()
         onBall4Click()
     }, 2000);
 }
 
 function saveState() {
-    var elBall1 = document.querySelector(".ball1");
-    var elBall2 = document.querySelector(".ball2");
+    const { elBall1, elBall2 } = getBalls();
     var body = document.querySelector("body");
 
     balls = [{ radius: parseInt(getComputedStyle(elBall1).width), backgroundColor: getComputedStyle(elBall1).backgroundColor },
@@ -150,57 +160,23 @@ function saveState() {
     }
 }
 
-function undo(elUndo) {
-    var lastState = undoStates.pop();
-    var body = document.querySelector("body");
+function applyState(state) {
+    const { elBall1, elBall2 } = getBalls();
 
-    console.log(balls)
+    elBall1.style.width = state.balls[0].radius + "px";
+    elBall1.style.height = state.balls[0].radius + "px";
+    elBall1.style.backgroundColor = state.balls[0].backgroundColor;
 
-    if (undoStates.length === 0) {
-        elUndo.disabled = true
-    }
-    const redoBtn = document.querySelector(".redo-btn");
-    redoBtn.disabled = false
-    var elBall1 = document.querySelector(".ball1");
-    var elBall2 = document.querySelector(".ball2");
-    var body = document.querySelector("body");
+    elBall2.style.width = state.balls[1].radius + "px";
+    elBall2.style.height = state.balls[1].radius + "px";
+    elBall2.style.backgroundColor = state.balls[1].backgroundColor;
 
-    redoStates.push(saveState())
-    console.log("currentWidth " + getComputedStyle(elBall1).width)
-    console.log("lastState " + lastState.balls[0].radius)
-    elBall1.style.width = lastState.balls[0].radius + "px";
-    elBall1.style.height = lastState.balls[0].radius + "px"
-    elBall1.style.backgroundColor = lastState.balls[0].backgroundColor
-    elBall2.style.width = lastState.balls[1].radius + "px";
-    elBall2.style.height = lastState.balls[1].radius + "px"
-    elBall2.style.backgroundColor = lastState.balls[1].backgroundColor
-    body.style.backgroundColor = lastState.backgroundColor
-
-    console.log(lastState)
+    document.body.style.backgroundColor = state.backgroundColor;
 }
 
-function redo(elRedo) {
-    if (redoStates.length != 0) {
-        var lastState = redoStates.pop();
-        elRedo.disabled = (redoStates.length === 0)
-
-        var elBall1 = document.querySelector(".ball1");
-        var elBall2 = document.querySelector(".ball2");
-        var body = document.querySelector("body");
-
-        undoStates.push(saveState())
-        const undoBtn = document.querySelector(".undo-btn");
-        undoBtn.disabled = (undoStates.length === 0)
-
-        console.log("currentWidth " + getComputedStyle(elBall1).width)
-        console.log("lastState " + lastState.balls[0].radius)
-        elBall1.style.width = lastState.balls[0].radius + "px";
-        elBall1.style.height = lastState.balls[0].radius + "px"
-        elBall1.style.backgroundColor = lastState.balls[0].backgroundColor
-        elBall2.style.width = lastState.balls[1].radius + "px";
-        elBall2.style.height = lastState.balls[1].radius + "px"
-        elBall2.style.backgroundColor = lastState.balls[1].backgroundColor
-        body.style.backgroundColor = lastState.backgroundColor
-        console.log(lastState)
-    }
+function getBalls() {
+    return {
+        elBall1: document.querySelector(".ball1"),
+        elBall2: document.querySelector(".ball2")
+    };
 }
